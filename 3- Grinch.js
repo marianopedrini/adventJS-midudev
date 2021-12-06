6703cb5873c78d7f498408a2378a7034dab87539
marianopedrini <pedrini.mariano96@gmail.com>
2021-12-06 15:56:01 -0300

    Finished challenge 3

--- 3- Grinch.js
+++ 3- Grinch.js
@@ -1,4 +1,35 @@
-export default function isValid(letter) {
- // ¡No dejes que el Grinch gane!
- return true
+const letter1 = 'bici coche (balón) bici coche peluche'; // -> ✅
+const letter2 = '(muñeca) consola bici'; // ✅
+
+const letter3 = 'bici coche (balón bici coche'; // -> ❌
+const letter4 = 'peluche (bici [coche) bici coche balón'; // -> ❌
+const letter5 = '(peluche {) bici'; // -> ❌
+const letter6 = '() bici'; // ❌
+const letter7 = '(()) bici'; // ❌
+
+function isValid(letter) {
+  if (
+    letter.includes('{') ||
+    letter.includes('}') ||
+    letter.includes('[') ||
+    letter.includes(']') ||
+    letter.match(/\(\)/)
+  ) {
+    return false;
+  }
+
+  if (letter.match(/\(.*?/)) {
+    if (letter.match(/\(([^)]+)\)/)) {
+      return true;
+    }
+    return false;
+  }
 }
+
+console.log(isValid(letter1));
+console.log(isValid(letter2));
+console.log(isValid(letter3));
+console.log(isValid(letter4));
+console.log(isValid(letter5));
+console.log(isValid(letter6));
+console.log(isValid(letter7));
